3ca8c482c7a093c025090e09e5fa187b5965f7b1
ابوالفضل علی پور <alypwr573@gmail.com>
2024-08-29 21:29:59 +0330

    add _ in check_password func

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from models.all_models import Reseller
+from models.all_models import Reseller ,Product , Customer
 
 
-
-r1 = Reseller("brand #1",'logo #1','1','2','3','4' )
-print(r1)
+p1 = Product(1,13,140000)
+r1 = Reseller("brand #1",'logo #1',p1,'1','2','3','4' )
+for i in range(100):
+    Customer(1,2,34,5)
+print(Customer.con)
--- models/all_models.py
+++ models/all_models.py
@@ -10,31 +10,24 @@ class User:
 
 
 
-class Customer:
+class Customer (User):
     counter = 0   #class attrs
 
     def __init__(self,username,password,fullname,email):
         self.wallet_amount = 0 
         self.is_enable = False
+        
+        Customer.counter+=1
         super().__init__(username,password,fullname,email)
 
 
     def __str__(self):
         return self.username
 
-    def check_password(self,password):
+    def _check_password(self,password):
         return self.password == password
     
     def set_enable(self):
         self.enable = True
 
-
-class Reseller(User):
-    def __init__(self, brand,logo,*args,**kwargs):
-        self.brand = brand
-        self.logo = logo
-        super().__init__(*args, **kwargs)
-
-    def __str__(self) :
-        return self.username
 
