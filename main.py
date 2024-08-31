fe0c4654e1b00d965386e3ac907263f287ac8914
ابوالفضل علی پور <alypwr573@gmail.com>
2024-08-31 10:57:37 +0330

    init commit for Multiple inheritance and method execution order in parent classes

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from models.all_models import Customer
+class Apartment:
+    pass
 
+class House:
+    pass
 
+class Rent:
+    pass
+class Sale:
+    pass
 
-
-Customer.singup("1",234545676,3,4)
-Customer.singup("1",234545676,3,4)
-Customer.singup("1",234545676,3,4)
-Customer.singup("1",234545676,3,4)
-Customer.singup("1",234545676,3,4)
-print(Customer.counter)
