f03047095fe00ea6b313813073830219a31b424a
ابوالفضل علی پور <alypwr573@gmail.com>
2024-08-31 12:43:48 +0330

    None

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 class Apartment:
-    pass
+    def __init__(self,floor,elevator) :
+        self.floor = floor
+        self.elevator = elevator
 
 class House:
-    pass
+    def __init__(self,age,address) :
+        self.age = age
+        self.address = address
 
 class Rent:
-    pass
-class Sale:
-    pass
+    def __init__(self,fixed_amount,monthly_amount):
+        self.fixed_amount = fixed_amount
+        self.monthly_amount = monthly_amount
 
+class Sale:
+    
+
+ 
